72c9f6129b46b7be4323375159c285c520fb12c2
Stanislaw Stepak <stanislawstepak@outlook.com>
2018-11-15 13:04:44 +0100

    Function to parametrize waves

---
+++ Stachu.py
@@ -0,0 +1,20 @@
+from python_speech_features import mfcc
+from collections import defaultdict
+from os import listdir
+
+sample_data = [('A01M1_0_', [1,2,3,4], 48),
+        ('A01M1_1_', [1, 2, 3, 4,5,6], 48),
+        ('BC1M1_0_', [1, 2, 3, 4], 48),
+        ('BC1M1_1_', [1, 2, 3, 4, 5, 6], 48)]
+
+
+def get_params(data):
+    ret = defaultdict(lambda: [])
+    for el in data:
+        filename = el[0].split('_')[0]
+        number = el[0].split('_')[1]
+        assert(len(filename) == 5 and len(number) == 1)
+        mfcc_ = mfcc(el[1], samplerate=el[2], appendEnergy=True) # add paremeters
+        ret[filename].append((mfcc_, number))
+    return ret
+
---
+++ main.py
@@ -0,0 +1,4 @@
+# MAIN FILE containing only calls to functions
+from .Stachu import *
+
+# get_params(data)
