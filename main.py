cdcd9b4fd02ebe51bc90c0bad678e39596b274e0
Kuba <jakub.wrobel94@gmail.com>
2018-11-18 18:29:09 +0100

    Updated main with some example

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 # MAIN FILE containing only calls to functions
 from Stachu import get_params
 from Mati import read_files
+from Kuba import get_labels_dictionary
+from Kuba import get_gmm_models
+from Kuba import show_model_and_data
 
-matrix = read_files()
-print(matrix)
+file_list= read_files()
+params_dict = get_params(file_list)
+labels_dict = get_labels_dictionary(params_dict)
+gmm_models = get_gmm_models(labels_dict, 20, 200)
+show_model_and_data(labels_dict, gmm_models, '8', 1, [-100, 100])
