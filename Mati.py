92cb1df69dbba85816425bb5de44a688ec999d90
MielonkaZateusz <45081926+MielonkaZateusz@users.noreply.github.com>
2018-11-25 13:31:06 +0100

    Update Mati.py

--- Mati.py
+++ Mati.py
@@ -12,11 +12,31 @@ def read_files():
     return matrix_of_param
 
 
-def divide_data_set_to_cross_validate(params_dict):
+def divide_data_set_to_cross_validate(params_dict,number_of_pairs):
     # change this function to return correct values as it is now
+
     key_list = list(params_dict.keys())
-    train_keys = key_list[0:17]
-    test_keys = key_list[17:23]
-    train_set_params = dict((k, params_dict[k]) for k in train_keys if k in params_dict)
+    print("Key list :", key_list)
+    train_keys = list()
+    test_keys = list()
+    j = 0
+    for i in list(itertools.permutations(params_dict.keys(),number_of_pairs)):
+
+        if j > number_of_pairs:
+            break
+        else:
+            train_keys.append(i)
+            j = j + 1
+
+    for i in len(key_list):
+
+        test_keys.append(list(set(key_list) - set(train_keys[i])))
+
+
+    print("Train keys", train_keys)
+    print("Test keys", test_keys)
+
+
+    train_set_params = dict((k,params_dict[k]) for k in train_keys if k in params_dict)
     test_set_params = dict((k, params_dict[k]) for k in test_keys if k in params_dict)
     return train_set_params, test_set_params
